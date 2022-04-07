bc81c422d232eaeae2ea4573a12838dd7eb361e5
Pedro Reis Velloso dos Santos <pedro_reis14@hotmail.com>
2022-04-07 17:28:18 -0300

    feat: 🎸 about page content

---
+++ app/ui/components/accordion.tsx
@@ -0,0 +1,26 @@
+import { Disclosure } from '@headlessui/react'
+import { FaChevronDown } from 'react-icons/fa'
+
+interface AccordionProps {
+  title: string
+}
+
+export const Accordion: React.FC<AccordionProps> = ({ title, children }) => {
+  return (
+    <Disclosure>
+      {({ open }) => (
+        <div className="border border-[#0b061c] flex flex-col">
+          <Disclosure.Button className="py-2 px-4 text-left flex items-center justify-between bg-[#0b061c]">
+            <span>{title}</span>
+            <FaChevronDown
+              className={`${open ? 'transform rotate-180' : ''} transition-all`}
+            />
+          </Disclosure.Button>
+          <Disclosure.Panel className="py-2 px-4 text-neutral-400">
+            {children}
+          </Disclosure.Panel>
+        </div>
+      )}
+    </Disclosure>
+  )
+}
